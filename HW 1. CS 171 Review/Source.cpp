469ad20d11028a532a18ca85d9cd2b7280ae89d5
smoreno19 <smoreno19@LAB-LIB-223-20>
2016-09-15 13:53:49 -0700

    Start up

---
+++ HW 1. CS 171 Review/Source.cpp
@@ -0,0 +1,67 @@
+//// 
+// Silvia Moreno
+// HW 1 - CS 171 Review
+// 9/8/16
+////
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void ex02();
+void ex03();
+
+int main() {
+	ex02();
+	ex03();
+	return 0;
+
+}
+
+void ex02() {
+
+	int hasPassedTest;
+	hasPassedTest = true;
+
+
+	int x = rand() % 10;
+	int y = rand() % 10;
+	if (x >= y)
+		cout << "The number for x, " << x << " is greater than the number for  y " << y << endl;
+
+	int numberOfShares;
+	int value;
+	cout << "Please enter a value: /n";
+	cin >> value;
+	if (value < 100)
+		cout << "The value is less than 100." << value;
+
+	int bookWidth, boxWidth, answer;
+	cout << "Please enter a width of a book:\n";
+	cout << "Please enter a width of a box:\n";
+	cin >> bookWidth;
+	cin >> boxWidth;
+	answer = boxWidth / bookWidth;
+	if (answer % 2)
+		cout << "It is evenly divisible";
+
+
+	int shelfLife, outsideTemp, resultShelfLife;
+	shelfLife = 0;
+	outsideTemp = 0;
+	resultShelfLife = 0;
+	cout << "Please enter a shelf life for a box of chocolate: ";
+	cin >> shelfLife;
+	cout << "Please enter the temperature outside: ";
+	cin >> outsideTemp;
+	if (outsideTemp > 90)
+		resultShelfLife = shelfLife - 4;
+
+
+
+
+}
+void ex03() {
+
+}
